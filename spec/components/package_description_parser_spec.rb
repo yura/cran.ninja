16f2509d87bb676c1bb2a15e5ea4aebe9001e4e1
Yury Kotlyarov <yura@brainhouse.ru>
2019-06-22 19:05:24 +0700

    refactor: Use treetop-dcf for parsing package descriptions

--- spec/components/package_description_parser_spec.rb
+++ spec/components/package_description_parser_spec.rb
@@ -4,34 +4,34 @@ RSpec.describe PackageDescriptionParser do
   describe '#parse' do
     subject(:parse) { described_class.new.parse(file_content) }
 
-    let(:file_content) { File.open('spec/fixtures/DESCRIPTION') }
+    let(:file_content) { File.open('spec/fixtures/DESCRIPTION').read }
 
     it 'parses package name' do
-      expect(parse[:name]).to eq('AcuityView')
+      expect(parse['Package']).to eq('AcuityView')
     end
 
     it 'parses title' do
-      expect(parse[:title]).to eq("A Package for Displaying Visual Scenes as They May Appear to an\nAnimal with Lower Acuity")
+      expect(parse['Title']).to eq("A Package for Displaying Visual Scenes as They May Appear to an Animal with Lower Acuity")
     end
 
     it 'parses version' do
-      expect(parse[:version]).to eq('0.1')
+      expect(parse['Version']).to eq('0.1')
     end
 
     it 'parses version' do
-      expect(parse[:date]).to eq('2017-04-28')
+      expect(parse['Date/Publication']).to eq('2017-05-09 07:10:48 UTC')
     end
 
     it 'parses description' do
-      expect(parse[:description]).to eq('This code provides a simple method for representing a visual scene as it may be seen by an animal with less acute vision. When using (or for more information), please cite the original publication.')
+      expect(parse['Description']).to eq('This code provides a simple method for representing a visual scene as it may be seen by an animal with less acute vision. When using (or for more information), please cite the original publication.')
     end
 
     it 'parses authors' do
-      expect(parse[:author]).to eq("Eleanor Caves [aut, cre],\nSönke Johnsen [aut]")
+      expect(parse['Author']).to eq("Eleanor Caves [aut, cre], \tSönke Johnsen [aut]")
     end
 
     it 'parses maintainer' do
-      expect(parse[:maintainer]).to eq('Eleanor Caves <eleanor.caves@gmail.com>')
+      expect(parse['Maintainer']).to eq('Eleanor Caves <eleanor.caves@gmail.com>')
     end
   end
 end
