4109d42940078f2b635e9a32c047ab1e3ed4e7b8
Yury Kotlyarov <yura@brainhouse.ru>
2019-06-22 16:07:27 +0700

    lost file

--- app/components/packages_parser.rb
+++ app/components/packages_parser.rb
@@ -1,22 +1,14 @@
+require 'dcf'
+
 # Parses CRAN PACKAGES file.
 class PackagesParser
 
   # Returns list of hashes with following keys:
-  #  * name - package name
-  #  * version
+  #  * Package - package name
+  #  * Version
   def parse(file_content)
-    result = []
-    package = {}
-    file_content.each do |line|
-      if line =~ /^Package: (.+)$/
-        package[:name] = $1
-      elsif line =~ /^Version: (.+)$/
-        package[:version] = $1
-        result << package
-        package = {}
-      end
+    Dcf.parse(file_content).map do |record|
+      record.slice("Package", "Version")
     end
-
-    result
   end
 end
