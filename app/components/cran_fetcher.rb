1eb58cb132f3fa6f8047f79a09e99e7e8ac35680
Yury Kotlyarov <yura@brainhouse.ru>
2019-06-22 16:22:41 +0700

    feat: Fetch package description file

--- app/components/cran_fetcher.rb
+++ app/components/cran_fetcher.rb
@@ -1,3 +1,7 @@
+require 'rubygems/package'
+require 'zlib'
+require 'open-uri'
+
 class CranFetcher
   attr_reader :cran_server
 
@@ -5,7 +9,27 @@ class CranFetcher
     @cran_server = 'http://cran.r-project.org/src/contrib/'
   end
 
+  def fetch(count = nil)
+    packages_file_content = fetch_packages_file
+
+    result = []
+    PackagesParser.new.parse(packages_file_content).each_with_index do |package, i|
+      break if count && i == count
+
+      result << fetch_package_description(name: package['Package'], version: package['Version'])
+    end
+  end
+
   def fetch_packages_file
     open(File.join(cran_server, 'PACKAGES')).read
+  end
+
+  def fetch_package_description(name:, version:)
+    tar = Gem::Package::TarReader.new(Zlib::GzipReader.new(open(File.join(cran_server, "#{name}_#{version}.tar.gz"))))
+    tar.rewind
+    result = tar.detect { |entry| entry.full_name == "#{name}/DESCRIPTION" }.read
+    tar.close
+
+    result
   end
 end
--- spec/components/cran_fetcher_spec.rb
+++ spec/components/cran_fetcher_spec.rb
@@ -4,6 +4,28 @@ RSpec.describe CranFetcher do
   let(:cran_fetcher) { described_class.new }
   let(:cran_server) { 'http://cran.r-project.org/src/contrib/' }
 
+  describe '#fetch' do
+    subject(:fetch) { cran_fetcher.fetch(50) }
+
+    let(:packages_file_content) { File.open('spec/fixtures/PACKAGES').read }
+    let(:package_description_content) { File.open('spec/fixtures/DESCRIPTION').read }
+
+    before do
+      allow(cran_fetcher).to receive(:fetch_packages_file).and_return(packages_file_content)
+      allow(cran_fetcher).to receive(:fetch_package_description).with(any_args).exactly(5).times
+    end
+
+    it 'fetches list of packages' do
+      fetch
+      expect(cran_fetcher).to have_received(:fetch_packages_file)
+    end
+
+    it 'fetches description of each file' do
+      fetch
+      expect(cran_fetcher).to have_received(:fetch_package_description).with(name: 'abc.data', version: '1.0')
+    end
+  end
+
   describe '#fetch_packages_file' do
     subject(:fetch_packages_file) { cran_fetcher.fetch_packages_file }
 
@@ -23,6 +45,28 @@ RSpec.describe CranFetcher do
     it 'reads the file' do
       fetch_packages_file
       expect(cran_uri).to have_received(:read)
+    end
+  end
+
+  describe '#fetch_package_description' do
+    subject(:fetch_package_description) { cran_fetcher.fetch_package_description(name: 'AWR', version: '1.11.189') }
+
+    let(:package_url) { cran_server + 'AWR_1.11.189.tar.gz' }
+    let(:cran_uri) { double('cran_uri') }
+    let(:package_file) { File.open('spec/fixtures/AWR_1.11.189.tar.gz') }
+    let(:package_description) { File.open('spec/fixtures/AWR/DESCRIPTION').read }
+
+    before do
+      allow(cran_fetcher).to receive(:open).with(package_url).and_return(package_file)
+    end
+
+    it 'requests package file' do
+      fetch_package_description
+      expect(cran_fetcher).to have_received(:open).with('http://cran.r-project.org/src/contrib/AWR_1.11.189.tar.gz')
+    end
+
+    it 'reads DESCRIPTION file from the package' do
+      expect(fetch_package_description).to eq(package_description)
     end
   end
 end
